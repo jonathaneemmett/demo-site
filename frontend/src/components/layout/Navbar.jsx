65e3476ee52f48bd4688720d0224718e0b05fb91
Jonathan E. Emmett <jon@emmettmedia.com>
2022-07-04 13:14:58 -0500

    added fix for dropdown not closing when link is clicked

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 
@@ -19,20 +19,24 @@ import { useStyles } from "../../assets/styles";
 const Navbar = ({user: {user}, logOut}) => {
     const classes = useStyles();
 
+    // To handle closing of menu when link is clicked, primarily for mobile view or dropdowns.
+    const [isChecked, setIsChecked] = useState(false);
+
     // funcs
     const handleLogout = async (e) => {
         e.preventDefault();
         await logOut();
+        setIsChecked(!isChecked);
     }
-
+    console.log(isChecked);
     return (
         <nav className={classes.navbar}>
             <div className={classes.logo}><Link to="/">Demo Site</Link></div>
             <ul className={classes.navLinks}>
-                <input type="checkbox" name="checkboxToggle" id="checkboxToggle"/>
+                <input type="checkbox" name="checkboxToggle" id="checkboxToggle" checked={isChecked} onChange={() => setIsChecked(!isChecked)}/>
                 <label htmlFor="checkboxToggle" className={classes.hamburger}><FaBars /></label>
                 <div className={classes.menu}>
-                    {user ? <li><a onClick={(e) => handleLogout(e)}>Logout</a></li> : <li><Link to="/login">Login</Link></li>}
+                    {user ? <li><a onClick={(e) => handleLogout(e)}>Logout</a></li> : <li><Link to="/login" onClick={() => setIsChecked(!isChecked)}>Login</Link></li>}
                 </div>
             </ul>
         </nav>
